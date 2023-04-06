d9fdd9c4e6eed0c2c70cd52a7dc695ee5db8040a
Felipe Perlato <fe_perlato@hotmail.com>
2023-04-06 12:22:08 -0300

    primeiro commit

---
+++ Arquivos2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string caminho = @"C:\Users\Felipe\Desktop\PROJETOS\file1.txt";
+
+        FileStream fs = null;
+
+        StreamReader sr = null;
+
+        try
+        {
+
+            sr = File.OpenText(caminho);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            
+
+            
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine("ocorreu um erro");
+            Console.WriteLine(e.Message);
+
+        }
+
+       
+        {
+            if (sr != null) sr.Close();
+            if (fs != null) fs.Close();
+        }
+    }
+}
